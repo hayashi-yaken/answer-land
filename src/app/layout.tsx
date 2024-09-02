71fbce098c33b823e47c3693a7c9ec75f9a6f56e
hayashi-yaken <mypodo@icloud.com>
2024-09-03 06:33:07 +0900

    linter, formatter の設定変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
